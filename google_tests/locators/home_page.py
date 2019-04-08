7377d862774ef1e90fa7ea56097d1768787a804b
Marek Martyniak <ms.martyniak@gmail.com>
2019-04-08 14:53:52 +0200

    Aktualizacja selektora dla przycisku szukaj

--- google_tests/locators/home_page.py
+++ google_tests/locators/home_page.py
@@ -5,5 +5,5 @@ from selenium.webdriver.common.by import By
 class HomePageLocators:
     GOOGLE_LOGO = By.ID, 'hplogo'
     SEARCH_BAR = By.NAME, 'q'
-    SEARCH_BUTTON = By.NAME, 'btnG'
+    SEARCH_BUTTON = By.CSS_SELECTOR, '*[name="btnG"],*[name="btnK"]'
     FEELING_LUCKY_BUTTON = By.NAME, 'btnI'

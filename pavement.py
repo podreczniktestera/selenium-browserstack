9290eba46c892506e12e4e5e0660715004c22b09
Marek Martyniak <ms.martyniak@gmail.com>
2019-04-08 13:37:10 +0200

    Aktualizacja pavement

--- pavement.py
+++ pavement.py
@@ -14,19 +14,19 @@ setup(
     packages=['features']
 )
 
-def run_behave_test(config, feature, task_id=0):
-    sh('CONFIG_FILE=config/%s.json TASK_ID=%s behave features/%s.feature' % (config, task_id, feature))
+def run_behave_test(config, task_id=0):
+    sh('CONFIG_FILE=config/%s.json TASK_ID=%s behave' % (config, task_id))
 
 @task
 @consume_nargs(1)
 def run(args):
     """Run single, local and parallel test using different config."""
     if args[0] in ('single', 'local'):
-        run_behave_test(args[0], args[0])
+        run_behave_test(args[0])
     else:
         jobs = []
         for i in range(4):
-            p = multiprocessing.Process(target=run_behave_test, args=(args[0], "single", i))
+            p = multiprocessing.Process(target=run_behave_test, args=(args[0], i))
             jobs.append(p)
             p.start()
 
